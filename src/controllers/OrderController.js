a05c761cb5bba03c7e2165487780f3f3e29a005c
Victor Lima <victorlimacar@gmail.com>
2024-02-10 22:25:29 -0300

    Inserindo orders

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -18,6 +18,33 @@ class OrderController {
         }
     }
 
+    static async postOrder(req, res) {
+
+        try {
+            const auth = req.headers['authorization'];
+            const user_id = await this.getUserByToken(auth);
+            const { order_data, date, available} = req.body;
+
+            const query = "insert into orders (order_data, date, available, user_id) VALUES ($1, $2, $3, $4)";
+
+            await db.query(query, [order_data, date, available, user_id]);
+
+            res.status(201).json({
+                "Message": "order inserida com sucesso"
+            })
+        } catch (error) {
+            console.error('Erro na consulta ao banco de dados:', error);
+
+            if (error.code === '23502') {
+                // Código de erro '23502' geralmente indica uma violação de NOT NULL (coluna obrigatória)
+                res.status(400).json({ error: 'Erro: A coluna: ' + error.column + " é obrigatória" });
+            }
+            else {
+                res.status(500).json({ error: error });
+            }
+        }
+    }
+
     static async getUserByToken(auth) {
         const query = `SELECT user_id FROM authorizations where token = '${auth}' limit 1`;
         const user_id = await db.query(query);
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const OrderController = require('./../controllers/OrderController.js');
 
 router.get("/", OrderController.getOrders.bind(OrderController));
+router.post("/", OrderController.postOrder.bind(OrderController));
 
 
 module.exports = router; 
